067e2f985efb639818861a79471fed1783f7a460
NTTPS <teamerze@gmail.com>
2023-10-04 15:38:21 +0700

    ีfix color mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,6 +14,9 @@ export default defineNuxtConfig({
         // CSS file in the project
         "@/assets/css/main.css",
     ],
+    colorMode: {
+        preference: "light",
+    },
     runtimeConfig: {
         public: {
             version: "",
