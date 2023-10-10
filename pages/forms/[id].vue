e982343fea6f59836bfa0cba3ee09bc58058ce22
NTTPS <teamerze@gmail.com>
2023-10-10 10:55:44 +0700

    update page info

--- pages/forms/[id].vue
+++ pages/forms/[id].vue
@@ -1,13 +1,16 @@
 <template>
-    <div>
+    <PartialsTitle prefix="ระบบ" title="แบบสอบถาม" icon="i-mdi-vote" back/>
 
+    <div class="px-8 mt-4">
+      
     </div>
 </template>
 
 <script setup>
-
+    const submit = () => {
+        navigateTo('/')
+    }
 </script>
 
 <style lang="scss" scoped>
-
 </style>
