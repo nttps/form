410b713d4a1824c54896c8d9313dcf668f5e6cef
NTTPS <teamerze@gmail.com>
2024-02-28 16:05:59 +0700

    set height

--- layouts/guest.vue
+++ layouts/guest.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-gradient-to-r from-white to-[#B2B4B7] flex flex-col min-h-screen">
-    <main class="min-h-[calc(100vh-75px)] h-[calc(100vh-75px)] lg:min-h-[calc(100vh-5rem)] lg:h-[calc(100vh-5rem)] flex flex-col">
+    <main class="min-h-[calc(100vh-75px)] lg:min-h-[calc(100vh-5rem)] flex flex-col h-full">
         <slot />
     </main>
     <PartialsFooter />
