0d2b5a18cc92eef3e707cfc228f02791bcea7761
NTTPS <teamerze@gmail.com>
2024-03-07 21:08:46 +0700

    fix fontsize

--- components/Partials/Header.vue
+++ components/Partials/Header.vue
@@ -16,7 +16,7 @@
             <div>
                 <a href="/" class="flex justify-center items-center">
                     <img src="~/assets/images/logo.png" class="w-[70px]" alt="E-Booking Logo">
-                    <div class="text-lg 2xl:text-xl ml-4">
+                    <div class="text-ปส 2xl:text-2xl ml-4">
                         <div>ระบบโหวตและแบบสอบถาม</div> 
                         <div class="text-[#FFA133]">กรมป้องกันและบรรเทาสาธารณภัย</div> 
                     </div>
